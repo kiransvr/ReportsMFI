4e80550e162b64c87a64465db722f4bc6aa77c48
Vangeti Bhanu Prakash Reddy <60293758+bhanureddy2105@users.noreply.github.com>
2024-04-04 17:29:33 -0400

    minor change

--- reports/src/app/app.routes.ts
+++ reports/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'forms',
+        path: '',
         // canActivate: [authGuardFn],
         loadChildren: () => import('./forms/forms.module').then(m => m.FormsModule)
     },
